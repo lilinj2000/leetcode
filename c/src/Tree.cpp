29eccf73cd4e9689d9f551b14a471a8acecb0c9b
lilinj2000 <lilinj2000@163.com>
2014-11-23 13:38:14 +0000

    the connect of the tree link node

--- c/src/Tree.cpp
+++ c/src/Tree.cpp
@@ -158,3 +158,22 @@ std::vector<std::vector<int> > Tree::levelOrderBottom(TreeNode *root)
 
   return res;
 }
+
+void Tree::connect(TreeLinkNode* root)
+{
+  if( !root )
+    return;
+
+  if( root->left )
+  {
+    root->left->next = root->right;
+  }
+
+  if( root->right && root->next )
+  {
+    root->right->next = root->next->left;
+  }
+
+  connect(root->left);
+  connect(root->right);
+}
